d3a0df437e4b81f4bb64817cf1c6c616005678a8
vinnythering <106132318+vinnythering@users.noreply.github.com>
2022-05-24 21:31:14 -0700

    Update child.h

--- child.h
+++ child.h
@@ -1,1 +1,34 @@
+#pragma once
+#include <iostream>
+#include "classes.h"
+#include "child.h"
+#include <string>
 
+using namespace std;
+
+class appliance {
+	private:
+		
+		
+	public:
+		appliance();
+		appliance();
+};
+
+class digitalMedia {
+	private:
+		
+		
+	public:
+		digitalMedia();
+		digitalMedia();
+};
+
+class furniture {
+	private:
+		
+		
+	public:
+		furniture();
+		furniture();
+};
