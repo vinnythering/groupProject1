1947ca316b2491f4037e669b62e3832163344d87
vinnythering <106132318+vinnythering@users.noreply.github.com>
2022-05-24 19:50:52 -0700

    Update and rename RetailItem.h to classes.h

---
+++ classes.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class inventory {
+	private:
+		double markupPercent;
+		double price;
+		double cost;
+		const double tax = 9.5;
+		
+		
+	public:
+		inventory();
+		inventory();
+};
+
+class item {
+	private:
+		int sku;
+		int unitsOnHand;
+		string itemDescription;
+		double price;
+	
+	public:
+		item();
+		item(string, int , double);
+
+		void setDescription(string);
+		string getDescription();
+		int getUnitsOnHand();
+		void setUnitsOnHand(int);
+		double getPrice();
+		void setPrice(double);
+		int getSKU();
+		void setSKU(int);
+};
