eba7ab7f745fd2b24e9f1c5bce25a7293cd505d8
vinnythering <106132318+vinnythering@users.noreply.github.com>
2022-05-28 20:49:36 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -17,39 +17,7 @@ int sku();
 int main()
 
 {
-	double p = 0;
-	int u = 0;
-	string d; 
 	
-	cout << "Welcome";
-	cout << "\nPlease enter the price for item 1:";
-	while (p <= 0)
-	{
-		cin >> p;
-		if (p <= 0)
-		{
-			cout << "\nPlease enter a valid number.";
-		}	
-	}
-	cout << "\nInventory must be greater than 0";
-	cout << "\nPlease enter the units on hand for item 1:";
-	while (u <= 0)
-	{
-		cin >> u;
-		if (u <= 0)
-		{
-			cout << "\nPlease enter a valid number.";
-		}	
-	}
-	cout << "\n Please enter the description for item 1:";
-	cin >> d;
-	cout << "\nDisplay all items:";
-	item obj1(d, u, p);
-	cout << "\nDescription" << obj1.getDescription() << "\nunits:" << obj1.getUnitsOnHand() << "\nPrice:" << obj1.getPrice();
-	item obj2("Jeans", 40, 34.95);
-	cout << "\nDescription" << obj2.getDescription() << "\nunits:" << obj2.getUnitsOnHand() << "\nPrice:" << obj2.getPrice();
-
-	system("pause");
 	return 0;
 
 }
@@ -58,37 +26,41 @@ void mainMenu()
 {
 	int choice = 0;
 	
-	cout << "Main Menu" << endl << endl;
+	cout << "Restaurant Management Main Menu" << endl << endl;
 	cout << "Enter a number to choose a menu option." << endl;
-	cout << "1) Enter new item." << endl;
-	cout << "2) Enter an order." << endl;
-	cout << "3) Sale" << endl;
-	cout << "4) Print complete report." << endl;
+	cout << "1) Cash handling." << endl;
+	cout << "2) Employee information." << endl;
+	cout << "3) Inventory." << endl;
+	cout << "4) Sales." << endl;
 	cout << "5) Quit." << endl << endl;
 	cout << "Enter: ";
+	
 	do
 	{
 		cin >> choice;
 		if (choice <= 0 || choice > 5)
 		{
+			cout << endl;
 			cout << "Please enter a valid option: ";
 			continue;
 		}
-	}
+	} while (choice <=0 || choice > 5);
+	
+	cout << endl;
 	
 	switch (choice)
 	{
 		case 1:
-			newItem();
+			cashHandling();
 			break;
 		case 2:
-			order();
+			employeeInformation();
 			break;
 		case 3:
-			sale();
+			inventory();
 			break;
 		case 4:
-			printReport();
+			sales();
 			break;
 		case 5:
 			quitProgram();
@@ -96,7 +68,7 @@ void mainMenu()
 	}
 }
 
-void newItem()
+void cashHandling()
 {
 	int choice = 0;
 	
